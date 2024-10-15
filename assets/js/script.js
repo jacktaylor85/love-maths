7ce48e38808655f0f419500749fe1598021cd098
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-10-15 14:58:37 +0000

    add js for buttons, start making random number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-console.log("Connected!");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+if (this.getAttribute("data-type") === "submit") {
+    alert("You Clicked Submit!");
+} else {
+    let gameType = this.getAttribute("data-type");
+    alert(`You clicked ${gameType}`);
+}
+        })
+    }
+})
+
+function runGame() {
+let num1 = Math.floor(Math.random()* 25)+1;
+let num2 = Math.floor(Math.random()* 25)+1;
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
