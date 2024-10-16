0fac7a9621e623fe906fa734375ece03a980ac40
Jack Taylor <jackandrewtaylor1@gmail.com>
2024-10-16 12:18:40 +0000

    display correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,16 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+let operand1 = parseInt(document.getElementById('operand1').innerText)
+let operand2 = parseInt(document.getElementById('operand2').innerText)
+let operator = document.getElementById("operator").innerText;
 
+if (operator === "++") {
+return [operand1, operand2, "addition"];
+} else {
+    alert(`Unimplemeneted operator ${operator}.`)
+    throw `Unimplemeneted operator ${operator}. Aborting!`
+}
 }
 
 function incrementScore() {
